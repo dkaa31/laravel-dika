18266f781826ae2053ca42067389313f38c6e187
Aljudika Riziq Juhansyah <dikajuhansyah@gmail.com>
2025-01-09 14:40:09 +0700

    awal

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <h1>SELAMAT DATANG!!</h1>
+        <H2>Terima kasih telah bergabung di Website Kami. Media belajar kita bersama!</H2>
+    </div>
+    <video width="1000px" autoplay muted  loop>
+        <source src="{{ asset('image/thx.mp4') }}" type="video/mp4">
+    </video>
+        
+    <img src="{{ asset('image/fotokucing.jpg') }}" alt="Gambar" width="450px" height="563px">
+</body>
+</html>
